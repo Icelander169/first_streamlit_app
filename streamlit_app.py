8d5dc8e36b84eca9966023dcb3867cc6559a9340
Icelander169 <82467822+Icelander169@users.noreply.github.com>
2023-01-23 10:27:58 +0100

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("My Breakfast Menu")
-🥣 streamlit.text("Omega 3 & Blueberry Oatmeal")
-🥗streamlit.text("Kale, Spinach & Rocket Smoothie")
-🐔streamlit.text("Hard-Boiled Free-Range Egg")
-🥑🍞streamlit.text("Avocado Toast")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.text("Hello There! I'm hungry!")
   
