564c3137b622bf923dd3c9f64a67b9afdb5e6598
Icelander169 <82467822+Icelander169@users.noreply.github.com>
2023-01-23 10:27:16 +0100

    Update streamlit_app.py
    added some emoijs

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("My Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+🥣 streamlit.text("Omega 3 & Blueberry Oatmeal")
+🥗streamlit.text("Kale, Spinach & Rocket Smoothie")
+🐔streamlit.text("Hard-Boiled Free-Range Egg")
+🥑🍞streamlit.text("Avocado Toast")
 
 streamlit.text("Hello There! I'm hungry!")
+  
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
